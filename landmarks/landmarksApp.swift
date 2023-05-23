85562e3cdae620526b9b5fbfa2bf1f73f383b430
Sheng Chou <chousheng@gmail.com>
2023-05-23 13:55:56 -0700

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Sheng Chou on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
